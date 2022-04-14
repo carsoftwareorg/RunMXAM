1cceb976544bd5fbfa93bc0e46c265a3e010946b
Di <di.peng@cariad.technology>
2022-04-14 07:32:32 +0200

    PMTMBSESDK-659 fix bug in MXAMBugs

--- script/main.m
+++ script/main.m
@@ -1,8 +1,8 @@
-% strArtifact = 'MXAM_Test';
-% strArtifactDir = '.\testdata\MXAM\';
-% strChecksetDir = 'C:\Users\DevInf-2\Documents\projects\matlab-test\script\MXAM_Checks_v7.3.0\MXAM_Checks';
-% strCheckset = 'projects\ef8\HCP1\hcp1_chassis_fixpoint.mxmp';
-% strMxamDir = 'C:\Tools\MXAM\7_2_0';
+ strArtifact = 'MXAM_Test';
+ strArtifactDir = 'C:\Users\MBSD.SDK\Desktop\Repo\RunMXAM\script\MXAM';
+ strChecksetDir = 'C:\Users\MBSD.SDK\Desktop\Repo\RunMXAM\script\MXAM_Checks_v7.3.0\MXAM_Checks';
+ strCheckset = 'projects\ef8\HCP1\hcp1_chassis_fixpoint.mxmp';
+ strMxamDir = 'C:\Tools\MXAM\7_2_0';
 
 disp(strMxamDir);
 disp(strArtifact);
